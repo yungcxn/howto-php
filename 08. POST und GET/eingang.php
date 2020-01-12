20778565ed9a724a511fa506ddebf68c5db4f4da
Can <efecan@Davuts-MBP.fritz.box>
2020-01-12 22:08:17 +0100

    v3

--- 08. POST und GET/eingang.php
+++ 08. POST und GET/eingang.php
@@ -7,11 +7,12 @@
   <body>
     <h1>PHP-Tutorial</h1>
     <p>Hallo</p>
-    <?php
 
-    
-
-    ?>
+    <form action="seite2.php" method="post">
+    Vorname: <input type="text" name="vorname" /><br />
+    Namename: <input type="text" name="nachname" /><br />
+    <input type="Submit" value="Absenden" />
+    </form>
 
     <p>Ciao</p>
 
